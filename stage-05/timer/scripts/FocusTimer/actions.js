546ba893326dd112602a373246cb89169bbc7cff
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-07-16 23:00:15 -0300

    desenvolvendo a logica do timer

--- stage-05/timer/scripts/FocusTimer/actions.js
+++ stage-05/timer/scripts/FocusTimer/actions.js
@@ -1,15 +1,17 @@
 import state from './state.js'
+import * as timer from './timer.js'
 
 export function toggleRunning(){
   state.isRunning = document.documentElement.classList.toggle
   ('running')
-  console.log(state)
-
+  
+  timer.countDown()
 }
 
 export function reset(){
   state.isRunning = false
   document.documentElement.classList.remove('running')
+  timer.updateDisplay()
 }
 
 export function set(){
