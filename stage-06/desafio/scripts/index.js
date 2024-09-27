e0d8e0ce8904d9868b6c661a45508e9bf41eec89
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-09-26 21:46:59 -0300

    costumização concluida

--- stage-06/desafio/scripts/index.js
+++ stage-06/desafio/scripts/index.js
@@ -7,10 +7,11 @@ const preLink = '/stage-06/desafio/pages'
 
 // nova instancia do objeto Router e que será usado a partir daqui
 const router = new Router()
+
 //criando/adicionando as rotas para garantir mais flexibilidade e manutenção mais simples
-router.add('/', `${preLink}/home.html`)
-router.add('/the-universe', `${preLink}/universe.html`)
-router.add('/exploration', `${preLink}/exploration.html`)
+router.add('/', `${preLink}/home.html`,'home')
+router.add('/the-universe', `${preLink}/universe.html`, 'universe')
+router.add('/exploration', `${preLink}/exploration.html`,'exploration')
 
 //para já começar a renderizando a home atraves do js
 router.handle()
--- stage-06/desafio/scripts/router.js
+++ stage-06/desafio/scripts/router.js
@@ -1,35 +1,40 @@
 export class Router {
   routes = {}
 
-  add(routeName, link){
-    this.routes[routeName] = link
+  add(routeName, link, className) {
+    this.routes[routeName] = { page: link, bodyClass: className }
   }
 
   route(event) {
     event = event || window.event
     event.preventDefault()
-  
+
     window.history.pushState({}, '', event.target.href)
     this.handle()
+  }
+
+  teste() {
+    console.log(this.routes['/'])
   }
 
   handle() {
     //desestruturação de dados
     const { pathname } = window.location
     const route = this.routes[pathname] || this.routes['/']
-  
-    fetch(route)
-    .then(data => data.text())
-    .then(html => {
-      document.querySelector('#app').innerHTML = html
-      this.addEventListeners()
-    })
+
+    fetch(route.page)
+      .then(data => data.text())
+      .then(html => {
+        document.querySelector('#app').innerHTML = html
+        document.body.className = route.bodyClass //ajusta a classe ao body para alterar a imagem de fundo
+        this.addEventListeners()
+      })
   }
+
   //para percorrer tambem links dentro das paginas e nao somente o index.hmtl inicial
   addEventListeners() {
     document.querySelectorAll('#app a').forEach(link => {
       link.addEventListener('click', event => this.route(event))
     })
   }
-
 }
