28f5ca1c252a651b36c2d8335684b8ed56f5ae97
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-10-17 21:32:11 -0300

    aplicação node iniciada

---
+++ stage-08/aula_01/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/message/:id/:user", (request, response)=>{
+  // desestruturação
+  const {id, user} = request.params
+  response.send(`
+    Mensagem ID: ${id}.
+    Para o usuario :${user}`)
+})
+
+
+const PORT = 3333
+app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
