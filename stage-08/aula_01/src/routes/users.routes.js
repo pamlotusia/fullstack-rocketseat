ce96c38c0fce6e67d6671736d81350aa8e2cf338
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-10-19 12:18:57 -0300

    controllers criados

--- stage-08/aula_01/src/routes/users.routes.js
+++ stage-08/aula_01/src/routes/users.routes.js
@@ -1,14 +1,14 @@
 // desentruturação -> *extraindo* o metodo Router do express ao inves de importar o modulo inteiro e todos os seus metodos
 const { Router } = require('express')
+const UsersController = require("../controllers/UsersController")
+
+const usersController = new UsersController()
 
 // cria um conjunto de rotas
 const usersRoutes = Router()
 
 // METODO POST
-usersRoutes.post('/', (request, response) => {
-  const { name, email, password } = request.body
-  response.json({ name, email, password })
-})
+usersRoutes.post('/', usersController.create)
 
 // exporta todo o conjunto de rotas usersRoutes
 module.exports = usersRoutes
