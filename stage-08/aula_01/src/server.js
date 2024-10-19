74cd36fff13a96bed946200744e5e686de5bf1b2
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-10-19 10:19:46 -0300

    metodo post

--- stage-08/aula_01/src/server.js
+++ stage-08/aula_01/src/server.js
@@ -1,11 +1,14 @@
-const express = require("express")
-
+const express = require('express')
 const app = express()
 
-app.get("/message/:id/:user", (request, response)=>{
+// definindo para a aplicação o formato de dados que ela vai lidar -> JSON
+app.use(express.json())
+
+// METODO GET
+app.get('/message/:id/:user', (request, response) => {
   //Route Param -> parametros obrigatorios
   // desestruturação
-  const {id, user} = request.params
+  const { id, user } = request.params
   response.send(`
     Mensagem ID: ${id}.
     Para o usuario :${user}`)
@@ -16,10 +19,20 @@ app.get("/message/:id/:user", (request, response)=>{
 // http://localhost:3333/users?page=2&limit=10
 // servidor/rota?parametro=valor&parametro=valor
 // sinal de ? para sinalizar query param, sinal de & para colocar mais de um na mesma rota
-app.get("/users", (request, response)=> {
-  const {page, limit} = request.query
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
   response.send(`Pagina: ${page}. Limite: ${limit}`)
 })
 
+// METODO POST
+app.post('/users', (request, response) => {
+  const { name, email, password } = request.body
+  /* devolve um tipo de html 
+  response.send({name, email, password})
+  */
+  // devolve um json -> formato padrão utilizado nas APIs
+  response.json({ name, email, password })
+})
+
 const PORT = 3333
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
