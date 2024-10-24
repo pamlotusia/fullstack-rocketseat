ac8afd4a3738eabde6a0427d0e8a2ce3b2ace91c
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-10-24 20:27:30 -0300

    validando email

--- stage-08/aula_01/src/controllers/UsersController.js
+++ stage-08/aula_01/src/controllers/UsersController.js
@@ -45,8 +45,8 @@ class UsersController{
       throw new AppError("Este e-mail já está em uso.")
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name
+    user.email = email ?? user.email
 
     if(password && !old_password){
       throw new AppError("Você precisa responder a senha antiga para redefinir a senha.")
