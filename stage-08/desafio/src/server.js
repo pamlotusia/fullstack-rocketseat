b8dcedcdb7839b5b2e0d7e8e6552ad51f063bec9
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-11-20 20:13:57 -0300

    configuação de banco de dados feita e funcionando

--- stage-08/desafio/src/server.js
+++ stage-08/desafio/src/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const routes = require("./routes")
+const database = require("./database/sqlite")
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
+database()
 
 const PORT = 4444
 app.listen(PORT , ()=> console.log(`Server is running on port ${PORT}`))
