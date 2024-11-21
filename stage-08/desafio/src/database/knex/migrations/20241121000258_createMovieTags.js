06710815a020d7ae806316b25cabd30165b6db92
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-11-20 21:19:34 -0300

    migrations criadas, e nao testadas

---
+++ stage-08/desafio/src/database/knex/migrations/20241121000258_createMovieTags.js
@@ -0,0 +1,11 @@
+
+exports.up = knex => knex.schema.createTable("movie_tags", table => {
+  table.increments("id")
+  table.text("name")
+
+  //foreign keys
+  table.integer("note_id").references("id").inTable("movie_notes").onDelete("CASCADE")
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE")
+})
+
+exports.down = knex = knex.schema.dropTable("movie_tags")
