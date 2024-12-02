f4a6f8ea87534b4ed997960b17bde1b368042b98
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-12-01 21:27:24 -0300

    Componente marcadores feito

--- stage-09/rocketnotes/src/pages/Details/index.jsx
+++ stage-09/rocketnotes/src/pages/Details/index.jsx
@@ -1,11 +1,23 @@
-import { Container } from './styles'
+import { Container, Links } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
     <Container>
-      <Header></Header>
+      <Header/>
+      <Section title="Links úteis">
+        <Links>
+        <li><a href="#">Link 1</a></li>
+        </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tag title = "express"></Tag>
+        <Tag title = "nodejs"></Tag>
+
+      </Section>
       <Button title='Voltar'/>
     </Container>
   )
