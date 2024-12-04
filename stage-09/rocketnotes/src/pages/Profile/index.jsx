2127f0a34bdbb50e1a48b7be0dde1feaab0b11e5
Pamela Lima <pamela.jesus.lima@gmail.com>
2024-12-04 19:56:23 -0300

    page profile concluida

--- stage-09/rocketnotes/src/pages/Profile/index.jsx
+++ stage-09/rocketnotes/src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi'
-import { Container, Form } from './styles'
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+import { Container, Form, Avatar } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
@@ -12,6 +12,16 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img src="https://github.com/pamlotusia.png" alt="Foto do usuário" />
+
+          <label htmlFor="avatar">
+            <FiCamera/>
+
+            <input type="file" id='avatar'/>
+          </label>
+          
+        </Avatar>
         <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="E-mail" type="text" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
